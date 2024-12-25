b2c78c799c1cc62e3930216bcb35c15b4b7d873a
Luis <luis.ruizi@cirvianum.cat>
2024-12-25 17:45:45 +0100

    Afegim serveis i la seva factory

---
+++ database/factories/ServiceFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Service>
+ */
+class ServiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->text(),
+            'price' => $this->faker->randomFloat(2, 0, 100),
+            'estimation' => $this->faker->randomFloat(2, 0, 100),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Client;
+use App\Models\Service;
 use App\Models\Shift;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,23 +19,25 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Luis',
-            'surname' => 'Perez',
-            'nick' => 'luisp',
-            'telf' => '123456789',
-            'email' => 'test@example.com',
-            'email_verified_at' => now(),
-            'password' => bcrypt('123456789'),
-            'remember_token' => Str::random(10),
-            'created_at' => now(),
-            'updated_at' => now(),
-            'dni' => '20572143T',
-            'is_admin' => 0,
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Luis',
+        //     'surname' => 'Perez',
+        //     'nick' => 'luisp',
+        //     'telf' => '123456789',
+        //     'email' => 'test@example.com',
+        //     'email_verified_at' => now(),
+        //     'password' => bcrypt('123456789'),
+        //     'remember_token' => Str::random(10),
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        //     'dni' => '20572143T',
+        //     'is_admin' => 0,
+        // ]);
 
-        Client::factory(3)->create();
+        // Client::factory(3)->create();
 
-        Shift::factory(1)->create();
+        // Shift::factory(1)->create();
+
+        Service::factory(3)->create();
     }
 }
