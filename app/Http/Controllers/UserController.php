a3fd8429266a901c0b12bfd4d9e275d904533058
Luis <luis.ruizi@cirvianum.cat>
2025-01-01 20:02:32 +0100

    Ajustem el UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use PhpParser\Node\Stmt\TryCatch;
+use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 
 class UserController extends Controller
@@ -49,7 +50,19 @@ class UserController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try {
+            $user = User::findOrFail($id);
+            return response()->json([
+                'status' => 'true',
+                'user' => $user,
+                'message' => 'Usuari trobat'
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Usuari no trobat'
+            ], 404);
+        }
     }
 
     /**
@@ -65,7 +78,38 @@ class UserController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'nick' => 'required|string|max:100',
+            'telf' => 'required|string|max:20',
+            'dni' => 'required|string|max:10',
+            'surname' => 'required|string|max:100',
+            'email' => 'required|string|max:100|email',
+            'password' => 'string|max:100',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Error en la validació de dades',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        try {
+            $user = User::findOrFail($id);
+            $user->update($request->all());
+            return response()->json([
+                'status' => true,
+                'data' => $user,
+                'message' => 'Usuari actualitzat correctament'
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Error al actualitzar l\'usuari'
+            ], 500);
+        }
     }
 
     /**
